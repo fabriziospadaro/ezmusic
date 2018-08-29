c007da5c17964fd053c0e47d8bff912088893be4
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-08-29 14:06:34 +0200

    v1.fix

--- app/services/youtube_scraper.rb
+++ app/services/youtube_scraper.rb
@@ -7,25 +7,20 @@ class YoutubeScraper
     name.gsub!(" ","+")
     #get the document
 
-    doc = Nokogiri::HTML(open("https://www.youtube.com/results?search_query=#{name}"))
+    doc = Nokogiri::HTML(open("https://www.youtube.com/results?sp=EgIQAQ%253D%253D&search_query=#{name}"))
     l = doc.xpath('//h3/a')
 
     data = []
     l.each do |t|
-      hash = {}
-      hash["name"] = t["title"]
-      hash["url"] = t["href"].sub("/watch?v=","")
-      data << hash
-    end
-
-    count = 0
-    doc.xpath('//ul').each do |j|
-      if(j["class"] == "yt-lockup-meta-info" && !j.children[1].nil?)
-        data[count]["views"] = j.children[1].text.split(" ")[0]
-        count = count+1
+      if(t["href"].include?("/watch?v="))
+        hash = {}
+        hash["name"] = t["title"]
+        hash["url"] = t["href"].sub("/watch?v=","")
+        data << hash
       end
     end
 
     return data
   end
 end
+
