504959f27dcdea6b7e7fadb706f31f285c39c208
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-09-04 19:20:17 +0200

    play best tracks

--- app/controllers/api/v1/scraper_controller.rb
+++ app/controllers/api/v1/scraper_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ScraperController < ApplicationController
       def get_video_urls
+        #a.gsub!(/[^0-9A-Za-z]/, '') rimuovi special hracter
         render json: {status: 'success', infos: YoutubeScraper.scrape(params[:name])}
       end
       def get_songs_like
